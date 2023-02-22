a9e59cc12a91644ade5b8831ab2df3d0adf82cbe
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-02-21 23:31:12 -0300

    analisis y paginado primer paso

--- api/main.py
+++ api/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI, Depends, HTTPException, status
 app = FastAPI(  tags=["PMBB"],
                 responses={status.HTTP_404_NOT_FOUND: {"message": "Not found"}})
 
-@app.get("/")
-async def root():
-    return "Hello FastAPI"
+@app.get("/products/", status_code=status.HTTP_200_OK)
+async def get_products(page: int | None = None):
+    #Retorna lista por defecto de productos, por ejemplo primeros 20 sin filtrar?
+    # Mostrar pagina 1 cuando no se especifica numero de pagina?
+    if page:
+        return {"page": page}
+    return {"page": 1}
